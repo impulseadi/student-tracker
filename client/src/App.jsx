c96b2a7b53fab32f658e5117dc1edad3b4cd4a8d
Aditya <adigupta.951@gmail.com>
2025-04-12 17:36:34 +0530

    Analytics added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Analytics } from "@vercel/analytics/react"
 import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
